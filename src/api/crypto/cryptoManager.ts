12545167e902f5c89311ec9fa4e3652f50632970
Menshons <menshons@gmail.com>
2022-08-03 20:29:12 +0200

    Crypto library edit

--- src/api/crypto/cryptoManager.ts
+++ src/api/crypto/cryptoManager.ts
@@ -1,15 +1,8 @@
-const crypt = require('crypto');
+const crypt = require('crypto-js');
 
-module.exports = {
-    encrypt: (data: string) => {
-        let cipher = crypt.createCipheriv('aes-256-cbc', process.env.AES_ENCRYPT_KEY, process.env.AES_INIT_VECTOR);
-        let encrypted = cipher.update(data, 'utf8', 'base64');
-        encrypted += cipher.final('base64');
-        return encrypted;
-    },
-    decrypt: (encryptedData: string) => {
-        let decipher = crypt.createDecipheriv('aes-256-cbc', process.env.AES_ENCRYPT_KEY, process.env.AES_INIT_VECTOR);
-        let decrypted = decipher.update(encryptedData, 'base64', 'utf8');
-        return (decrypted + decipher.final('utf8'));
-    }
+export function encrypt (data: string): string {
+    return crypt.AES.encrypt(data, crypt.enc.Utf8.parse(process.env.AES_ENCRYPT_KEY), {
+        iv: crypt.enc.Utf8.parse(process.env.AES_INIT_VECTOR),
+        mode: crypt.mode.CBC
+    }).toString();
 }
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors')
+
 const app = express();
+app.use(cors());
 
 const userRouter = require("./api/users/user.router")
 
